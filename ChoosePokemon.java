7b8c1a989adcd6e7d3f0782a084d1bd349339e29
CHE CHEOK HEI <75987638+ardee222@users.noreply.github.com>
2024-06-14 01:49:49 +0800

    a

--- ChoosePokemon.java
+++ ChoosePokemon.java
@@ -12,7 +12,7 @@ public class ChoosePokemon extends JFrame {
     private List<BufferedImage> imageList;
     private List<JLabel> pokemonLabels;
     private JPanel chosenPanel;
-    private String currentPokemon;
+    private int currentPokemonId;
     private List<Integer> chosenPokemonIds;
     // private 
 
@@ -24,17 +24,20 @@ public class ChoosePokemon extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // 設置框架的大小
-        setSize(1200, 800 );
+        setSize(1250, 1000 );
 
         // 初始化主面板並設置為 BorderLayout
         JPanel mainPanel = new JPanel(new BorderLayout());
 
+        //EAST
         //右邊的格子
-        Box eastBox = Box.createVerticalBox();
-        
+        // Box eastBox = Box.createVerticalBox();
+        JPanel eastBox = new JPanel(new GridLayout(2, 1));
         //box1 是當前Pokemon的技能
-        JLabel box1 = new JLabel("選擇你的Pokemon", SwingConstants.CENTER);
-        box1.setPreferredSize(new Dimension(400, 400));
+        JPanel box1 = new JPanel(new GridLayout(1, 2));
+        box1.setPreferredSize(new Dimension(600, 400));
+        box1.setBorder(new LineBorder(Color.BLACK));
+
         // 用戶所選的寶可夢
         Box chosen = Box.createVerticalBox();
         JLabel chosenBox1 = new JLabel("已選擇的Pokemon", SwingConstants.CENTER);
@@ -50,31 +53,36 @@ public class ChoosePokemon extends JFrame {
             icon = new ImageIcon(newimg);  // Transform it back
             JLabel label = new JLabel(icon);
             GridBagConstraints gbc = new GridBagConstraints();
-            gbc.insets = new Insets(10, 5, 10, 5);    
+            gbc.insets = new Insets(10, 0, 10, 0);    
             chosenBox2.add(label,gbc);
         }
+        chosen.setBorder(new LineBorder(Color.BLACK));
+        
 
-    
-
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(100, 0, 0, 0); 
         chosen.add(chosenBox1);
         chosen.add(chosenBox2);
-        chosen.setPreferredSize(new Dimension(400, 400));
+        chosen.setPreferredSize(new Dimension(600, 400));
         // eastBox.setBorder(BorderFactory.createTitledBorder("BoxLayout"));
         eastBox.add(box1);
         eastBox.add(chosen);
-        eastBox.setPreferredSize(new Dimension(600,  1000));
+        
+        eastBox.setPreferredSize(new Dimension(600,  800));
         // JPanel westBox = new JPanel(new GridLayout(6, 5));
 
         
-        
+        //NORTH
         JLabel northBox = new JLabel("選擇你的Pokemon", SwingConstants.CENTER);
         northBox.setPreferredSize(new Dimension(100, 100));
         northBox.setFont(new Font("Serif", Font.PLAIN, 20)); 
         
+
+        //WEST
         // pokemon 列表
         JPanel westBox = new JPanel(new GridLayout(6, 5));
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(10, 5, 10, 5);    
+        // GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);    
         
         for(int i=1; i<=30;i++){
             //載入圖片
@@ -112,7 +120,7 @@ public class ChoosePokemon extends JFrame {
                             Image newimg = image.getScaledInstance(100, 100,  java.awt.Image.SCALE_SMOOTH); // Scale it the smooth way
                             icon = new ImageIcon(newimg);  // Transform it back
                             JLabel label = new JLabel(icon);
-                            chosenBox2.add(label,gbc);
+                            chosenBox2.add(label);
                         }
                     }else{
                         JOptionPane.showMessageDialog(null, "Pokemon數量已逹上限" );
@@ -124,6 +132,18 @@ public class ChoosePokemon extends JFrame {
                 public void mouseEntered(MouseEvent e) {
                     JLabel enteredLabel = (JLabel) e.getSource();
                     enteredLabel.setBorder(new LineBorder(Color.RED));
+                    
+                    currentPokemonId = index;
+                    if(currentPokemonId != 0){
+                        // JLabel subbox1 = new JLabel("選擇你的Pokemon", SwingConstants.CENTER);
+                        ImageIcon icon = new ImageIcon("pokemon-data/image/"+currentPokemonId+".png");
+                        Image image = icon.getImage(); 
+                        Image newimg = image.getScaledInstance(100, 100,  java.awt.Image.SCALE_SMOOTH); 
+                        icon = new ImageIcon(newimg);  
+                        JLabel label = new JLabel(icon);
+                        box1.add(label);
+                    }
+                    System.out.println(currentPokemonId);
                     repaint();
                 }
 
@@ -131,6 +151,10 @@ public class ChoosePokemon extends JFrame {
                 public void mouseExited(MouseEvent e) {
                     JLabel exitedLabel = (JLabel) e.getSource();
                     exitedLabel.setBorder(null);
+                    currentPokemonId = 0;
+                    box1.removeAll();
+                    System.out.println(currentPokemonId);
+                    repaint();
                 }
             });
         }
